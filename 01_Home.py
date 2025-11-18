0d247d099e38161d5ec550fdcf775186b1ebc070
ZakariaDia <zakariabilaldia@gmail.com>
2025-11-18 14:34:59 -0500

    Update 01_Home.py

--- 01_Home.py
+++ 01_Home.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plost
 from PIL import Image
 
 
 st.logo("david.png",icon_image="david-logo.png",size="large")
 
+seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
 
 # Page setting
 
@@ -22,15 +24,29 @@ st.title("Bienvenue, <nom> !")
 
 st.subheader("Mesures principales")
 
-contMain = st.container(border=True)
+col1,col2,col3 = st.columns(3,border=True)
 
-with contMain:
+
+with col1:
     st.metric("Risque d'incendie","XX%","-X %")
     st.metric("Température X", "22.0 °C", "1.2 °C")
     st.metric("Humidité 1", "54%", "1.2 %")
 
-
-
+with col2:
+    graphs = st.container
+    st.markdown('### Histogramme de température')
+    plost.line_chart(
+    data=seattle_weather,
+    color = "#f73939",
+    x='date',
+    y='temp_max')
+with col3:
+    st.markdown('### Histogramme humidité')
+    plost.line_chart(
+    data=seattle_weather,
+    x='date',
+    y='temp_max',
+    color = "#1088e9",)
 
 
 
