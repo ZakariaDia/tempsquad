46e9bebd13184235e816d32a162abfdafa5e610f
ZakariaDia <zakariabilaldia@gmail.com>
2025-11-22 15:22:39 -0500

    live updates of charts + metrics
    pyplot is now graph generator, however problems exist:
    - user selects zoom, refresh causes the zoom to reset (caused by the graph being re-created every refresh, and not updating the data)
    -> to fix this, either find a way to create an empty graph and update the data every x interval, or switch to a different library that fixes this issue
    -> HOWEVER: must retain the capability to change background colour n stuff (problem with old library), and dimensions.

--- 01_Home.py
+++ 01_Home.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plost
 import plotly.express as pl
-from PIL import Image
-
-
-
 
 # Page setting
 
@@ -16,12 +10,12 @@ st.set_page_config(layout="wide")
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-
-
 st.title("Bienvenue, <nom> !")
-# Row A
 
+
+st.divider()
 st.subheader("Mesures principales")
+
 
 col1,col2,col3 = st.columns(3,border=True)
 
@@ -31,7 +25,7 @@ def createTemp():
     chart = pl.line(data, x="date", y="temp1",height=350)
     chart.update_layout(paper_bgcolor="#21499f",plot_bgcolor="#21499f")
     chart.update_traces(line_color="#DF3A40")
-    st.plotly_chart(chart,key="tempChart")
+    st.plotly_chart(chart,use_container_width=True,key="tempChart")
 
 @st.fragment(run_every="3s")
 def createHum():
@@ -40,11 +34,18 @@ def createHum():
     chart.update_layout(paper_bgcolor="#21499f",plot_bgcolor="#21499f")
     st.plotly_chart(chart,key="humChart")
 
+@st.fragment(run_every="3s")
+def createMetric():
+    data = pd.read_csv('measurements.csv', parse_dates=['date'])
+    lastRow = data.tail(1)
+    temp = lastRow.iloc[0,21]
+    hum = lastRow.iloc[0,22]
+    st.metric("Risque d'incendie","XX%","-X %")
+    st.metric("Température", temp, "1.2 °C")
+    st.metric("Humidité", hum, "1.2 %")
 
 with col1:
-    st.metric("Risque d'incendie","XX%","-X %")
-    st.metric("Température X", "22.0 °C", "1.2 °C")
-    st.metric("Humidité 1", "54%", "1.2 %")
+    createMetric()
     
 with col2:
     st.markdown('### Histogramme de température')
--- connector.py
+++ connector.py
@@ -4,7 +4,7 @@ import pandas as pd
 import datetime
 import random
  
-HOST = "192.168.229.214"  
+HOST = "10.35.255.214"  
                         
 PORT = 12345            
 
@@ -44,6 +44,22 @@ def generateHum(list):
         listRandom.append(element)
     return listRandom
 
+def generateMoy(list):
+    listMoy = []
+    totalTemp = 0
+    totalHum = 0
+    for i in range(len(list)):
+        if 12 > i > 3:
+            totalTemp = totalTemp + float(list[i])
+        elif i > 11:
+            totalHum = totalHum + float(list[i])
+    moyenneTemp = str(round((totalTemp + float(list[0]) + float(list[1]) )/ 10, 2))
+    moyenneHum = str(round((totalHum + float(list[2]) + float(list[3]) )/ 10, 2))
+    listMoy.append(moyenneTemp)
+    listMoy.append(moyenneHum) 
+
+    return listMoy
+
 def writer(file, list, cond):
     with open(file, "a",newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
@@ -61,6 +77,8 @@ def writer(file, list, cond):
                     secondHalf.append(f"tempSim{i-3}")
                 elif i<20:
                     secondHalf.append(f"humSim{i-11}")
+            secondHalf.append(f"tempMoy")
+            secondHalf.append(f"humMoy")
             header.extend(secondHalf) 
             print(header)   
             writer.writerow(header)
@@ -94,7 +112,8 @@ while True:
         response, server_address = mySocket.recvfrom(1024)
         variables = response.decode().split(",")
         print(variables)
-        variables = variables + generateTemps(variables)+generateHum(variables)
+        variables = variables + generateTemps(variables) + generateHum(variables)
+        variables = variables + generateMoy(variables)
         writer("measurements.csv",variables,isHeader)
         isHeader = False
     except socket.timeout:
