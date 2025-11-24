3591b32e0567b132f8dc89d533e638012bbbaf1a
alexi <alexi.lessard@gmail.com>
2025-11-23 23:05:43 -0500

    confidential
    dont worry abt it cuh

--- david.py
+++ david.py
@@ -5,6 +5,7 @@ import time
 st.markdown("# :rainbow[WELCOME TO DAVID LAND!!!]")
 
 st.image("david-logo.png",caption="DAVID!!!")
+st.image("chien.png",caption="this tha chicago grim reper")
 
 while True:
     st.balloons()
